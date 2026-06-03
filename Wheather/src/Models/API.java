5bb2b3050026099016b8463d95640fc9241d8e31
Victini-157 <WeiMa991@school.lu>
2026-06-03 11:02:48 +0200

    added a temperature class that has a hard codded temperature, a wheather class that outputs the desired degree (Celcius, Fahrenheit, Kelvin) and a jFrame called WheatherPanel that has a basic GUI

---
+++ Wheather/src/Models/API.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ *
+ * @author Markus
+ */
+public class API {
+    public static void main(String[] args){
+        ObjectMapper mapper = new ObjectMapper();
+    }
+}
