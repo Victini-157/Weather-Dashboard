e9ecb82d6027548046a2ee9d21efd262b40d0ab4
Victini-157 <WeiMa991@school.lu>
2026-06-12 11:57:30 +0200

    Working API caller

---
+++ Wheather/src/Models/Animation.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Markus
+ */
+public abstract class Animation {
+    
+    public abstract void draw(Graphics g);
+    public abstract void change();
+    
+}
