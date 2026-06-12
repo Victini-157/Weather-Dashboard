603fbfcef46f7700b2cecbb7996fb5616951e841
Victini-157 <WeiMa991@school.lu>
2026-06-12 09:52:10 +0200

    Working API caller

--- Wheather/src/Models/API.java
+++ Wheather/src/Models/API.java
@@ -3,14 +3,54 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Models;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import org.json.JSONObject;
+
 
 /**
  *
  * @author Markus
  */
 public class API {
-    public static void main(String[] args){
-        ObjectMapper mapper = new ObjectMapper();
+    private JsonHandler json= new JsonHandler();
+
+    public void getCelcius() throws MalformedURLException, IOException {
+
+        int celcius = 0;
+        String stringURL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,sunrise,sunset,daylight_duration,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,rain,snowfall,showers,snow_depth&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,rain,showers,snowfall";
+
+        URL url = new URL(stringURL);
+
+        Scanner scanner = new Scanner(url.openStream());
+        scanner.useDelimiter("\\A");
+
+        String jsonResponse = null;
+        if (scanner.hasNext()) {
+            jsonResponse = scanner.next();
+           
+            
+        }
+        scanner.close();
+        
+        JSONObject jo = new JSONObject(jsonResponse);
+        
+        
+        json.setTemperature_2m(jo.getJSONObject("current").getDouble("temperature_2m"));
+        System.out.println(json.getTemperature_2m());
+        
     }
+
+    public JsonHandler getJson() {
+        return json;
+    }
+
+ 
+
+    
+    
 }
