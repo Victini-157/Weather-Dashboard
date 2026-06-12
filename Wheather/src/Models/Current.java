4625c57ace6a48f5d9e6e2e9241eff6fd3593fee
Victini-157 <WeiMa991@school.lu>
2026-06-12 11:57:10 +0200

    Working API caller

---
+++ Wheather/src/Models/Current.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author Markus
+ */
+public class Current {
+    private String time;
+    private double interval, temperature_2m;
+
+    public String getTime() {
+        return time;
+    }
+
+    public double getInterval() {
+        return interval;
+    }
+
+    public double getTemperature_2m() {
+        return temperature_2m;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setInterval(double interval) {
+        this.interval = interval;
+    }
+
+    public void setTemperature_2m(double temperature_2m) {
+        this.temperature_2m = temperature_2m;
+    }
+    
+    
+    
+    
+}
